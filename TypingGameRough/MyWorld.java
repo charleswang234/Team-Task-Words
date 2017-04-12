42aacf8193e44e3dbb0971cfc8f763eaf1927f3e
yomanpwnage <Jadenhuang99@hotmail.com>
2017-04-12 17:42:16 -0400

    Changes to words and score
    asdasdas

--- TypingGameRough/MyWorld.java
+++ TypingGameRough/MyWorld.java
@@ -9,10 +9,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     final int randomSpawn = 400; //spawn rate of goombas
-    
     int count = 0; //Number of letter typed so far
-    Label label1 = new Label("Hello",50); //Word to be typed
+    Scoreboard score = new Scoreboard();
+    Label label1 = new Label("",50); //Word to be typed
     Label label2 = new Label("",50); //Displays what you are typing
+    Label label3 = new Label("Score: ",50);
     String key; //Tracks last key pressed
     String word1; //Tracks value of label1
     String word2; //Tracks value of label2
@@ -32,6 +33,8 @@ public class MyWorld extends World
      */
     private void prepare()
     {
+        score.score = 0; //Resets the current score;
+        
         Mario mario = new Mario();
         addObject(mario,475,638);
 
@@ -40,17 +43,22 @@ public class MyWorld extends World
 
         enemy enemy = new enemy(0);
         addObject(enemy,2,652);
-        
+
         addObject(label1,500,50);
         addObject(label2,500,350);
+        addObject(label3,100,450);
+        label1.setValue(words.randomWords());
+        label3.setValue("Score: " + score.score); //Sets score to 0
     }
-    
-     public void act(){
+
+    public void act(){
         key = Greenfoot.getKey();
         word1 = label1.getLabel();
         word2 = label2.getLabel();
         if(word2.equals(word1)){
             label2.setValue("");
+            score.score += 10;
+            label3.setValue(("Score: " + score.score));
             count = 0;
         }
         if(key != null){
@@ -60,5 +68,5 @@ public class MyWorld extends World
             }
         }
     }
-    
+
 }
